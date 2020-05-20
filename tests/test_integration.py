3f942117bc6234dc5a726cd9496fd645b48053b7
Hemant Pawar <hpawar@dwavesys.com>
2020-05-20 18:50:25 +0000

    Incorporated formatting issues

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,16 +1,16 @@
-#    Copyright 2020 D-Wave Systems Inc.
-
-#    Licensed under the Apache License, Version 2.0 (the "License")
-#    you may not use this file except in compliance with the License.
-#    You may obtain a copy of the License at
-
-#        http: // www.apache.org/licenses/LICENSE-2.0
-
-#    Unless required by applicable law or agreed to in writing, software
-#    distributed under the License is distributed on an "AS IS" BASIS,
-#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#    See the License for the specific language governing permissions and
-#    limitations under the License.
+# Copyright 2020 D-Wave Systems Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import subprocess
 import unittest
@@ -22,9 +22,9 @@ project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 class IntegrationTests(unittest.TestCase):
 
-    def runDemo(self,hardware):
+    def runDemo(self, hardware):
         demo_file = os.path.join(project_dir, 'demo.py')
-        output=subprocess.check_output([sys.executable, demo_file, hardware])
+        output = subprocess.check_output([sys.executable, demo_file, hardware])
         return str(output)
 
     def test_structural_imbalance_cpu(self):
@@ -32,7 +32,7 @@ class IntegrationTests(unittest.TestCase):
         output = self.runDemo("cpu")
         output = str(output).upper()
         if os.getenv('DEBUG_OUTPUT'):
-            print("Example output \n"+ output)
+            print("Example output \n" + output)
 
         with self.subTest(msg="Verify if output contains 'Created CSV file: Results/Global/Structural Imbalance.csv' \n"):
             self.assertIn("Created CSV file: Results/Global/Structural Imbalance.csv".upper(), output)
@@ -48,7 +48,7 @@ class IntegrationTests(unittest.TestCase):
         output = self.runDemo("qpu")
         output = str(output).upper()
         if os.getenv('DEBUG_OUTPUT'):
-            print("Example output \n"+ output)
+            print("Example output \n" + output)
 
         with self.subTest(msg="Verify if output contains 'Created CSV file: Results/Global/Structural Imbalance.csv' \n"):
             self.assertIn("Created CSV file: Results/Global/Structural Imbalance.csv".upper(), output)
