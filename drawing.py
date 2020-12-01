99f024c02dda3a28e71cc56d05b768120b0d754f
John McFarland <jmcfarland@dwavesys.com>
2020-12-01 15:45:13 -0800

    Remove unused imports

--- drawing.py
+++ drawing.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
-import os
 import matplotlib.pyplot as plt
 import networkx as nx
 
